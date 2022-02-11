4bca66fc10cbee8572a9a2ae16c2113d8cacb040
Lui3S4 <96251993+Lui3S4@users.noreply.github.com>
2022-02-10 23:49:09 -0300

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -53,16 +53,16 @@ class FunkinLua {
 		//trace('Lua version: ' + Lua.version());
 		//trace("LuaJIT version: " + Lua.versionJIT());
 
-		var result:Dynamic = LuaL.dofile(lua, script);
+		var result:Dynamic = LuaL.dofile(lua, modchart);
 		var resultStr:String = Lua.tostring(lua, result);
 		if(resultStr != null && result != 0) {
-			lime.app.Application.current.window.alert(resultStr, 'Error on .LUA script!');
-			trace('Error on .LUA script! ' + resultStr);
+			lime.app.Application.current.window.alert(resultStr, 'Error on .LUA modchart!');
+			trace('Error on .LUA modchart! ' + resultStr);
 			lua = null;
 			return;
 		}
-		scriptName = script;
-		trace('Lua file loaded succesfully:' + script);
+		scriptName = modchart;
+		trace('Lua file loaded succesfully:' + modchart);
 
 		#if (haxe >= "4.0.0")
 		accessedProps = new Map();
